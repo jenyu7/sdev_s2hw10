0a3b4369500f8160b061e1f2b8f286ed986517de
jenyu7 <jyu7@stuy.edu>
2018-03-07 21:42:00 -0500

    i think this works

--- clickdraw.js
+++ clickdraw.js
@@ -1,9 +1,9 @@
 /*
- * Team CLIQUE
- * Jen Yu, Shannon Lau, Vivien Lee
+ * Team yeyu
+ * Jen Yu and Helen Ye
  * SoftDev Period 7
- * 03-06-2018
- * K#09: Ask Circles [Change || Die]
+ * 03-07-2018
+ * K#10: Objectification
  */
 
 var svg = document.getElementById("slate");
@@ -14,59 +14,52 @@ var clear = document.getElementById("clear");
 
 console.log(clear);
 
-//initialized radius
-var r = 5;
-//growing?
-var grow = true;
-//animation id
-var id = 0;
-
-//clears the screen and resets the prevX and prevY variables
+//clears the screen
 var clearCallBack = function(e){
     while(svg.firstChild){
 	svg.removeChild(svg.firstChild);
     }
-};
-
-//draws a dot, returns the x and y coordinates in a list
-var drawDot = function(e){
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", e.offsetX);
-    c.setAttribute("cy", e.offsetY);
-    c.setAttribute("r", 10);
-    c.setAttribute("fill", "red");
-    c.setAttribute("stroke", "black");
-    c.addEventListener("click", changeColor);
-    svg.appendChild(c);
-    e.stopPropagation();
-};
-
-//toggles the color between red and blue
-var changeColor = function(e){
-    if(this.getAttribute("fill") == "red"){
-        this.setAttribute("fill", "blue");
-    }
-    else{
-	console.log("delete");
-	svg.removeChild(this);
-	randCircle();
-    }
-    e.stopPropagation();
 }
 
-//draws a circle at random coordinates
-var randCircle = function(e){
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", Math.random() * (width - 10));
-    c.setAttribute("cy", Math.random() * (height - 10));
-    c.setAttribute("r", 10);
-    c.setAttribute("fill", "red");
-    c.setAttribute("stroke", "black");
-    c.addEventListener("click", changeColor);
-    svg.appendChild(c);
+//creates a circle object w builtin fxns
+var drawDot = function(x, y, r){
+    var c = {
+	circle: document.createElementNS("http://www.w3.org/2000/svg", "circle"),
+	append: function(){
+	    svg.appendChild(this.circle);
+	},
+	changeColor: function(e){
+	    if(this.getAttribute("fill") == "red"){
+		this.setAttribute("fill", "blue");
+	    }
+	    else{
+		console.log("delete");
+		svg.removeChild(this);
+		rX = Math.random() * (width - 10);
+		rY = Math.random() * (height - 10);
+		svg.appendChild(drawDot(rX, rY, 20).circle);
+	    }
+	    e.stopPropagation();
+	}
+	
+    }
+    c.circle.setAttribute("cx", x);
+    c.circle.setAttribute("cy", y);
+    c.circle.setAttribute("r", r);
+    c.circle.setAttribute("fill", "red");
+    c.circle.setAttribute("stroke", "black");
+    c.circle.addEventListener("click", c.changeColor);
+    return c;
+}
+
+//when clicked
+var circle = function(e){
+    var c = drawDot(e.offsetX, e.offsetY, 20);
+    c.append();
+    e.stopPropagation();
 }
 
 //add the event listeners
-svg.addEventListener("click", drawDot);
+svg.addEventListener("click", circle);
 clear.addEventListener("click", clearCallBack);
 
